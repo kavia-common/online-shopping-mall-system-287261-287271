4078adcd9eaef6d8f0d4fa24a8d7904e35d5da19
Kavia AI <production@kavia.ai>
2025-12-06 20:16:47 +0000

    Checkpoint 1 - cg900f90c7

---
+++ .init/.linter.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/online-shopping-mall-system-287261-287271/springboot_backend
+./gradlew checkstyleMain
+LINT_EXIT_CODE=$?
+if [ $LINT_EXIT_CODE -ne 0 ]; then
+   exit 1
+fi
+
